f01f6a1d7a3d2bfc29ada4cfe2fa7c65a0c5263f
Jonas Flach <jonas.flach@uvaro.com>
2023-02-20 00:27:45 -0500

    Add existing project files to Git

---
+++ src/main/java/com/jonasqf/myexpenses/MyExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.jonasqf.myexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonasqf/myexpenses/MyExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasqf.myexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
